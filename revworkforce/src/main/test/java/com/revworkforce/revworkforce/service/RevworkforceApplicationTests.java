2189924a1dd9652fc589355405ad6393c3b8b84e
srilekha6619 <srilekhajanga19@gmail.com>
2026-03-11 03:13:29 +0530

    Add GoalServiceTest and RevworkforceApplicationTests

---
+++ revworkforce/src/main/test/java/com/revworkforce/revworkforce/service/RevworkforceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revworkforce.revworkforce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevworkforceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
