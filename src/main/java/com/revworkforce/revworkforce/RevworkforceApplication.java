67de0a4c875c6b979ddfd608e85532d26e003886
nenishkanth <nenishkanthb@gmail.com>
2026-03-10 21:28:15 +0530

    Initial project setup: Spring Boot scaffold, pom.xml, application.properties

---
+++ src/main/java/com/revworkforce/revworkforce/RevworkforceApplication.java
@@ -0,0 +1,13 @@
+package com.revworkforce.revworkforce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevworkforceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevworkforceApplication.class, args);
+	}
+
+}
